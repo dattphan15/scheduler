dd881355d6f230ae835fc27b8a37513a7122ecf0
Kevin <ken.phan@live.ca>
2021-03-16 14:41:59 -0700

    interviewClassItem

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,6 +24,7 @@ const days = [
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [interview, setInterview] = useState("");
 
   return (
     <main className="layout">
@@ -35,18 +36,17 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={days}
-          day={day}
-          setDay={setDay}
-        />
+          <DayList
+            days={days}
+            day={day}
+            setDay={setDay}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
-
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+const classNames = require('classnames');
+
+
+export default function InterviewerListItem(props) {
+  console.log(props);
+
+  const interviewerClass = classNames("interviewers__item", {
+     "interviewers__item--selected": props.selected,
+
+  });
+  return (
+    <li 
+      className={interviewerClass}
+      id={props.id}
+      name={props.name}
+      avatar={props.avatar}
+      onClick={props.setInterviewer}
+      >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+        {props.selected && props.name}
+    </li>
+  );
+}
