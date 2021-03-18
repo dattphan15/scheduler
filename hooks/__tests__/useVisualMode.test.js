3a5ad060ede6621f6d1163651088ae50ca55cf12
Kevin <ken.phan@live.ca>
2021-03-18 15:43:58 -0700

    render test

--- hooks/__tests__/useVisualMode.test.js
+++ hooks/__tests__/useVisualMode.test.js
@@ -1,11 +1,10 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks"
 
-import useVisualMode from "hooks/useVisualMode";
+import useVisualMode from "/home/kevin/lighthouse/w7-react/scheduler/hooks/useVisualMode.js";
 
 const FIRST = "FIRST";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
-
   expect(result.current.mode).toBe(FIRST);
 });
--- hooks/useVisualMode.js
+++ hooks/useVisualMode.js
@@ -1,9 +1,5 @@
 import { useState } from "react";
 
-// take in an initial mode
-// set the mode state with the initial mode provided
-// return an object with a mode property
-
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
 
