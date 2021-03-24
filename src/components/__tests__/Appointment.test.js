935ce7e8daf64800b8a6abec6a34455023f3f4f5
Kevin <ken.phan@live.ca>
2021-03-23 18:53:43 -0700

    change appointment test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -7,7 +7,7 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render, prettyDOM } from "@testing-library/react";
 
 /*
   We import the component that we are testing
@@ -18,13 +18,9 @@ import Appointment from "components/Appointment/index";
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
-});
-
-
 describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Appointment />);
+  it("renders without crashing", async () => {
+    // await render(<Application />);
+    console.log(prettyDOM(<Application />))
   });
 });
